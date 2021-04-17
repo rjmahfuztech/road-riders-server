702eda67632d8088cbf05a61d128c8265a03bbb2
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-04-18 01:48:46 +0600

    added more code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 
@@ -21,6 +22,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const serviceCollection = client.db("cycleService").collection("services");
   const adminCollection = client.db("cycleService").collection("admin");
+  const orderCollection = client.db("cycleService").collection("order");
+  const reviewCollection = client.db("cycleService").collection("review");
 
   // add service
   app.post('/addService', (req, res) => {
@@ -50,6 +53,49 @@ client.connect(err => {
     .toArray((err, data) => {
       res.send(data);
     })
+
+    // find specific service
+    app.get('/service/:id', (req, res) => {
+      const id = req.params.id;
+      serviceCollection.find({_id: ObjectId(id)})
+      .toArray((err, singleService) => {
+        res.send(singleService[0]);
+      })
+    })
+  })
+
+  // add order
+  app.post('/addOrder', (req, res) => {
+    const newOrder = req.body;
+    orderCollection.insertOne(newOrder)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+    })
+  })
+
+  // get order
+  app.get('/orders', (req, res) => {
+    orderCollection.find({})
+    .toArray((err, data) => {
+      res.send(data);
+    })
+  })
+
+  // add client review
+  app.post('/addReview', (req, res) => {
+    const newReview = req.body;
+    reviewCollection.insertOne(newReview)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+    })
+  })
+
+  // get review
+  app.get('/review', (req, res) => {
+    reviewCollection.find({})
+    .toArray((err, data) => {
+      res.send(data);
+    })
   })
 
   //add admin
@@ -60,12 +106,21 @@ client.connect(err => {
       res.send(result.insertedCount > 0);
     })
   })
+
+  // delete service
+  app.delete('/delete/:id', (req, res) => {
+    const deleteService = req.params.id;
+    serviceCollection.deleteOne({_id: ObjectId(deleteService)})
+    .then( result => {
+      res.send(result.deletedCount > 0);
+    })
+  })
   
 });
 
 
 app.get('/', (req, res) => {
-  res.send('Dy you find anything!!')
+  res.send('Do you find anything!!')
 })
 
 app.listen(process.env.PORT || port);
